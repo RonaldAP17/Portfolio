2229c7ce63e17d6044f8130914952e02b7790874
RonaldAP17 <ronaldocris324@gmail.com>
2021-10-28 10:55:37 -0500

    El primer commit con todos los archivos iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
+import { PresentationComponent } from './home/presentation/presentation.component';
+import { AboutMeComponent } from './home/about-me/about-me.component';
+import { KnowledgeComponent } from './home/knowledge/knowledge.component';
+import { ProjectsComponent } from './home/projects/projects.component';
+import { ContentComponent } from './home/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactComponent,
+    PresentationComponent,
+    AboutMeComponent,
+    KnowledgeComponent,
+    ProjectsComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
